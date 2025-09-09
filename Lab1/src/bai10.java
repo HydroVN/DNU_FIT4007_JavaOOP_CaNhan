d5c00102f0740f2cb79abbc538403eb116154cd9
HydroVN <duyvan921@gmail.com>
2025-09-09 20:43:24 +0700

    Commit

--- Lab1/src/bai10.java
+++ Lab1/src/bai10.java
@@ -4,7 +4,9 @@ public class bai10 {
     public static void main (String [] agrs) {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Nhập 1 ký tự thường (từ a đến y):");
-        char word = myScanner.next().charAt(0);
+        char word = myScanner.next().charAt(0); // .next(): đọc ký tự người dùng nhập
+        // .chartAt(0): trả về ký tự ở "At" số 0, là ký tự đầu tiên trong chuỗi
+        // Ví dụ: Người dùng nhập "abc" thì qua .charAt(0) thì nó sẽ lấy ký tự đầu là "a"
         if(word == 'z') {
             System.out.println("Không có ký tự nào sau " + word);
         }else {
