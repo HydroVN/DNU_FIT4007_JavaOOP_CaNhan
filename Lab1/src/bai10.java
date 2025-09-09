4fe114b8d7c0ba39c3ed93a66cee44db1db33c01
HydroVN <duyvan921@gmail.com>
2025-09-09 20:38:37 +0700

    Commit

--- Lab1/src/bai10.java
+++ Lab1/src/bai10.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class bai10 {
     public static void main (String [] agrs) {
         Scanner myScanner = new Scanner(System.in);
-
+        System.out.println("Nhập 1 ký tự thường (từ a đến y):");
+        char word = myScanner.next().charAt(0);
+        if(word == 'z') {
+            System.out.println("Không có ký tự nào sau " + word);
+        }else {
+            char newWord = (char) (word + 1);
+            System.out.println("Chữ cái liền sau của " + word + " là: " + newWord);
+        }
     }
 }
